9bcdfe96b54b6f53e143b2d33cb0cb29589f348c
jimin <dia0114@naver.com>
2024-09-10 22:04:45 +0900

    ADD: SideMenu와 SideMenuButton class를 추가

---
+++ src/SideMenu.py
@@ -0,0 +1,70 @@
+import customtkinter as ctk
+from typing import Callable
+
+
+class SideMenuButton(ctk.CTkButton):
+    _select_color: str
+    _original_fg_color: str
+
+    def __init__(self, master=None, select_color="transparent", **kw):
+        super().__init__(master, **kw)
+        self._select_color = select_color
+        self._original_fg_color = self.cget("fg_color")
+        self.configure(fg_color=self._select_color)
+
+    def select(self):
+        self.configure(fg_color=self._original_fg_color)
+
+    def deselect(self):
+        self.configure(fg_color=self._select_color)
+
+
+class SideMenu(ctk.CTkFrame):
+    inner_side_menu: ctk.CTkFrame
+    btns: list[ctk.CTkButton]
+    last_selected_btn: SideMenuButton
+
+    def _add_btn(self, text: str = "", on_click: Callable[[], None] = lambda: None):
+        btn = SideMenuButton(
+            self.inner_side_menu,
+            text=text,
+            height=50,
+            font=("Arial", 14),
+            corner_radius=0,
+            # fg_color="transparent",
+            select_color="transparent",
+        )
+
+        def command():
+            self.last_selected_btn.deselect()
+            btn.select()
+            self.last_selected_btn = btn
+            on_click()
+
+        btn.configure(command=command)
+        btn.grid(row=len(self.btns), column=0)
+        self.btns.append(btn)
+        return btn
+
+    def __init__(self, master=None, **kw):
+        super().__init__(master, **kw)
+        self.btns = []
+
+        self.inner_side_menu = ctk.CTkFrame(
+            self,
+            fg_color="transparent",
+        )
+        self.inner_side_menu.pack(side="left", fill="x")
+
+        self._add_btn("🏠 Home")
+
+        self._add_btn("🧔‍♂️ 회원관리")
+
+        test = self._add_btn("📗 도서관리")
+        print(test.cget("fg_color"))
+        # test.configure(fg_color=("default", "default"))
+
+        self._add_btn("⚙️ 설정")
+
+        self.last_selected_btn = test
+        test.select()
--- src/ctk_test.py
+++ src/ctk_test.py
@@ -1,32 +1,63 @@
 import customtkinter as ctk
+from SideMenu import SideMenu
 
-ctk.set_appearance_mode("light")
+ctk.set_appearance_mode("system")
+ctk.set_default_color_theme("green")
 app = ctk.CTk()
 app.geometry("800x600")
-app.resizable(False, False)
+# app.resizable(False, False)
 app.title("대출반납 관리 시스템")
 
 
-side_menu = ctk.CTkFrame(
-    app,
-)
+side_menu = SideMenu(app)
 side_menu.pack(side="left", fill="y")
 
-home_btn = ctk.CTkButton(
-    side_menu,
-    text="Home",
-    width=100,
-    height=50,
-)
-home_btn.grid(row=0, column=0, padx=10, pady=5)
+# inner_side_menu = ctk.CTkFrame(
+#     side_menu,
+#     fg_color="transparent",
+# )
+# inner_side_menu.pack(side="left", fill="x")
 
-about_btn = ctk.CTkButton(
-    side_menu,
-    text="About",
-    width=100,
-    height=50,
-)
-about_btn.grid(row=1, column=0, padx=10, pady=5)
+
+# home_btn = ctk.CTkButton(
+#     inner_side_menu,
+#     text="🏠 Home",
+#     height=50,
+#     font=("Arial", 14),
+#     corner_radius=0,
+#     fg_color="transparent",
+# )
+# home_btn.grid(row=0, column=0)
+
+# users_btn = ctk.CTkButton(
+#     inner_side_menu,
+#     text="🧔‍♂️ 회원관리",
+#     height=50,
+#     corner_radius=0,
+#     font=("Arial", 14),
+#     fg_color="transparent",
+# )
+# users_btn.grid(row=1, column=0)
+
+# books_btn = ctk.CTkButton(
+#     inner_side_menu,
+#     text="📗 도서관리",
+#     height=50,
+#     corner_radius=0,
+#     font=("Arial", 14),
+#     # fg_color="transparent",
+# )
+# books_btn.grid(row=2, column=0)
+
+# settings_btn = ctk.CTkButton(
+#     inner_side_menu,
+#     text="⚙️ 설정",
+#     height=50,
+#     corner_radius=0,
+#     font=("Arial", 14),
+#     fg_color="transparent",
+# )
+# settings_btn.grid(row=3, column=0)
 
 if __name__ == "__main__":
     app.mainloop()
